b60cbcb0860b8ce9843f8eb507706bc9f5d179b8
VickersCode <tyler31vickers@gmail.com>
2023-07-15 19:40:38 -0700

    add four squares (postIts)

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>  
+      <div className="App">
+        <Square />
+        <Square />
+      </div>
+      <div className='App'>
+        <Square />
+        <Square />
+      </div>
     </div>
   );
+}
+
+const Square = () => {
+  const squareStyle = {
+    display: 'flex',
+    flexDirection: 'row',
+    width: '100px',
+    height: '100px',
+    backgroundColor: 'yellow',
+    margin: '10px'
+  };
+
+  return <div style={squareStyle}></div>;
 }
 
 export default App;
